4aeee8905fa08660047e6d7b1462934ffe7b5a2b
Shreyas Jamkhandi <shreyxs@gmail.com>
2024-07-12 21:07:51 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import * as React from "react";
+import "@/lib/env";
+
+export default function HomePage() {
+	return (
+		<main>
+			<section className="bg-black min-h-[100vw] text-white mb-auto">
+				hi
+			</section>
+		</main>
+	);
+}
